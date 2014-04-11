0e52e00157b96dd292abffc79eda7c1590c8e8ee
Srikrishan Malik <srimalik@in.ibm.com>
2014-04-11 23:23:03 +0530

    Unlock before return.
    Signed-off-by: Jeremy Bongio <jbongio@us.ibm.com>
    Signed-off-by: Srikrishan Malik <srimalik@in.ibm.com>

--- src/SAL/state_lock.c
+++ src/SAL/state_lock.c
@@ -2881,18 +2881,25 @@ state_status_t state_unlock(cache_entry_t *entry, exportlist_t *export,
 	 */
 	PTHREAD_RWLOCK_wrlock(&entry->state_lock);
 
-	if (state->state_type == STATE_TYPE_DELEG
-	    && glist_empty(&entry->object.file.deleg_list)) {
-	  cache_inode_dec_pin_ref(entry, FALSE);
-	  LogDebug(COMPONENT_STATE,
-		   "Unlock success on file with no delegations");
-	  return STATE_SUCCESS;
+	/* The 9p protocols holds no state, just locks */
+	/* Opened files are managed on the client side */
+	if ((owner->so_type != STATE_LOCK_OWNER_9P) &&
+	    (state->state_type == STATE_TYPE_DELEG) &&
+	    glist_empty(&entry->object.file.deleg_list)) {
+		cache_inode_dec_pin_ref(entry, FALSE);
+		PTHREAD_RWLOCK_unlock(&entry->state_lock);
+		LogDebug(COMPONENT_STATE,
+			"Unlock success on file with no delegations");
+		return STATE_SUCCESS;
 	}
 
-	if (state->state_type == STATE_TYPE_DELEG) {
-	  status =
-	    subtract_deleg_from_list(entry, owner, state, &removed,
-				     &entry->object.file.deleg_list);
+	if ((owner->so_type != STATE_LOCK_OWNER_9P) &&
+	    (state->state_type == STATE_TYPE_DELEG)) {
+		LogFullDebug(COMPONENT_STATE, "Removing delegation from list");
+		status =
+			subtract_deleg_from_list(entry, owner, state, &removed,
+						&entry->object.file.deleg_list);
+		PTHREAD_RWLOCK_unlock(&entry->state_lock);
 	  return status;
 	}
 
