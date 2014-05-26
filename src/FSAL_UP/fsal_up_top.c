bedd4c1c965295f5dc82b153ebb9ece877e66fbf
Srikrishan Malik <srimalik@in.ibm.com>
2014-05-26 12:50:15 +0530

    Move CB channel state in channel struct.
    Signed-off-by: Srikrishan Malik <srimalik@in.ibm.com>

--- src/FSAL_UP/fsal_up_top.c
+++ src/FSAL_UP/fsal_up_top.c
@@ -1145,7 +1145,7 @@ static bool handle_badhandle_response(state_lock_entry_t *deleg_entry,
 
 		if (call->stat != RPC_SUCCESS) {
 			LogEvent(COMPONENT_NFS_CB, "Callback channel down");
-			p_cargs->clid->cb_chan_down = TRUE;
+			set_cb_chan_down(p_cargs->clid, true);
 			needs_revoke = TRUE;
 			break;	/* do not retry if the channel is down */
 		} else {
@@ -1274,7 +1274,7 @@ static int32_t delegrecall_completion_func(rpc_call_t *call,
 				nfs_cb_argop4_u.opcbrecall.fh.nfs_fh4_val;
 		if (call->stat != RPC_SUCCESS) {
 			LogEvent(COMPONENT_NFS_CB, "Callback channel down");
-			deleg_ctx->clid->cb_chan_down = TRUE;
+			set_cb_chan_down(deleg_ctx->clid, true);
 			needs_revoke = TRUE;
 		} else
 			needs_revoke = handle_recall_response(deleg_entry,
@@ -1366,7 +1366,7 @@ static uint32_t delegrecall_one(state_lock_entry_t *deleg_entry)
 	}
 
 	/* Attempt a recall only if channel state is UP */
-	if (clid->cb_chan_down == TRUE) {
+	if (get_cb_chan_down(clid)) {
 		LogCrit(COMPONENT_NFS_CB,
 			"Call back channel down, not issuing a recall");
 		code = NFS_CB_CALL_ABORTED;
@@ -1377,13 +1377,13 @@ static uint32_t delegrecall_one(state_lock_entry_t *deleg_entry)
 	if (!chan) {
 		LogCrit(COMPONENT_NFS_CB, "nfs_rpc_get_chan failed");
 		/* TODO: move this to nfs_rpc_get_chan ? */
-		clid->cb_chan_down = TRUE;
+		set_cb_chan_down(clid, true);
 		code = NFS_CB_CALL_ABORTED;
 		goto out;
 	}
 	if (!chan->clnt) {
 		LogCrit(COMPONENT_NFS_CB, "nfs_rpc_get_chan failed (no clnt)");
-		clid->cb_chan_down = TRUE;
+		set_cb_chan_down(clid, true);
 		code = NFS_CB_CALL_ABORTED;
 		goto out;
 	}
@@ -1655,7 +1655,6 @@ state_status_t delegrecall_impl(cache_entry_t *entry)
 		rc = delegrecall_one(deleg_entry);
 
 	}
-out:
 	PTHREAD_RWLOCK_unlock(&entry->state_lock);
 	return rc;
 }
